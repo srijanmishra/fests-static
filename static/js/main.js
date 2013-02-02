908e58934f8a425c9888b318d141ff260e259391
vj41 <vatsal.juneja1991@gmail.com>
2013-02-02 16:42:31 +0530

    Changed API uri for facebook registration.

--- static/js/main.js
+++ static/js/main.js
@@ -1,4 +1,4 @@
-var apiUrl = 'http://fests.herokuapp.com/api/v1/';
+var apiUrl = 'http://api.technex.in/api/v1/';
 var Events = null, Pages = null, Sections = null, sView = null;
 
 function slugify(text) {
